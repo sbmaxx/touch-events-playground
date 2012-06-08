fbadf9a96c0d36d45ad8c01151bd1fd901daf729
Roman sbmaxx Rozhdestvenskiy <sbmaxx@yandex-team.ru>
2012-06-08 14:50:55 +0400

    popup for jquery

--- pages/jquery/jquery.bemjson.js
+++ pages/jquery/jquery.bemjson.js
@@ -6,5 +6,13 @@
         { elem: 'js', url: 'http://yandex.st/jquery/1.7.2/jquery.min.js' },
         { elem: 'js', url: 'jquery.js' }
     ],
-    content: [{ block: 'b-test' }]
+    content: [
+        {
+            block: 'b-test'
+        },
+        {
+            block: 'b-test-popup',
+            js: true
+        }
+    ]
 })
